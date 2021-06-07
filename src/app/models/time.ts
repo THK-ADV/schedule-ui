93aa1b25153cc3a9c9d47de5879fd50c7b2b85e4
Alexander Dobrynin <info@alex-dobrynin.com>
2021-06-07 12:08:27 +0200

    adds time (LocalTime) as a type

---
+++ src/app/models/time.ts
@@ -0,0 +1,40 @@
+export interface Time {
+  date: Date
+  hour: number
+  minute: number
+  seconds: number
+}
+
+// parses numbers out of 'HH:mm:ss'. fallback are 0 for time values
+export const timeFromString = (timeString: string, date?: Date): Time => {
+  date = date || new Date(+0)
+  const time = splitToNumbers(timeString)
+  const h = time.shift() || 0
+  const m = time.shift() || 0
+  const s = time.shift() || 0
+
+  date = new Date(date.setHours(h, m, s))
+  return fromDate(date)
+}
+
+const splitToNumbers = (timeString: string): number[] =>
+  timeString
+    .split(':')
+    .map(Number)
+    .filter(isNumber)
+
+const isNumber = (value: any): value is number =>
+  typeof value === 'number'
+
+const fromDate = (date: Date): Time => ({
+  date,
+  hour: date.getHours(),
+  minute: date.getMinutes(),
+  seconds: date.getSeconds()
+})
+
+
+const withNewDate = (date: Date, time: Time): Time => {
+  date = new Date(date.setHours(time.hour, time.minute, time.seconds))
+  return fromDate(date)
+}
