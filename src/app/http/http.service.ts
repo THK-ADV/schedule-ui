124dfec4e3b823a475c3f8bf3935238884f1f9b2
Alexander Dobrynin <info@alex-dobrynin.com>
2021-12-09 14:58:32 +0100

    real CRUD for module with po (WIP) (#26)
    http request error will be shown in alert view

--- src/app/http/http.service.ts
+++ src/app/http/http.service.ts
@@ -1,8 +1,9 @@
 import {Injectable} from '@angular/core'
 import {HttpClient, HttpErrorResponse, HttpParams} from '@angular/common/http'
-import {Observable, throwError} from 'rxjs'
+import {EMPTY, Observable} from 'rxjs'
 import {catchError} from 'rxjs/operators'
 import {timeFromString} from '../models/time'
+import {Alert, AlertService} from '../structure/alert/alert.service'
 
 export const parseDateStartEndFromJSON = <A>(a: any): A => {
   const date = new Date(a.date)
@@ -17,25 +18,34 @@ export const parseDateStartEndFromJSON = <A>(a: any): A => {
 })
 export class HttpService {
 
-  constructor(private readonly http: HttpClient) {
+  constructor(
+    private readonly http: HttpClient,
+    private readonly alert: AlertService
+  ) {
   }
 
   private handleError = (error: HttpErrorResponse): Observable<never> => {
+    let alert: Alert
     if (error.status === 0) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error)
+      alert = {
+        type: 'danger',
+        body: {
+          kind: 'message',
+          value: `An error occurred: ${JSON.stringify(error.error)}`
+        }
+      }
     } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong.
-      console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`
-      )
+      alert = {
+        type: 'danger',
+        body: {
+          kind: 'message',
+          value: `Backend returned code: ${error.status} with message: ${JSON.stringify(error.error)}`
+        }
+      }
     }
-    // Return an observable with a user-facing error message.
-    return throwError(
-      'Something bad happened; please try again later.'
-    )
+    console.error(alert.body.value)
+    this.alert.reportAlert(alert)
+    return EMPTY
   }
 
   getAll = <A>(url: string, params: HttpParams): Observable<A[]> =>
@@ -43,7 +53,7 @@ export class HttpService {
       .pipe(catchError(this.handleError))
 
   post = <A>(url: string, body: unknown, params?: HttpParams): Observable<A[]> =>
-    this.http.post<A[]>(url, body, { params })
+    this.http.post<A[]>(url, body, {params})
       .pipe(catchError(this.handleError))
 
   delete = <A>(url: string): Observable<A> =>
